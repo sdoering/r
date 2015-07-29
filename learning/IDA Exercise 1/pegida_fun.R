84bd2cb83e38c0d2a69fd4ff2bae1258979c5d7f
Sven Döring <software@schriftrolle.de>
2015-07-29 21:27:23 +0200

    Update pegida_fun.R

--- learning/IDA Exercise 1/pegida_fun.R
+++ learning/IDA Exercise 1/pegida_fun.R
@@ -7,5 +7,5 @@ mtext("#[a-z]{2}gida = ...", side=3, adj=.1, line=1, cex=2, col=rgb(.4,.4,.4))
 barplot(runif(10,.5,1.5), col=rainbow(10), main="")
 mtext("#no[a-z]{2}gida = ...", side=3, adj=.1, line=1, cex=3)
 
-mtext("[schriftrolle.de via joyofdata.de]", side=0, adj=.9, line=.4, cex=.9)
+mtext("[via joyofdata.de]", side=0, adj=.9, line=.4, cex=.9)
 
